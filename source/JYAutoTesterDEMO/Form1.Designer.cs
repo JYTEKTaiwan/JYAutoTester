76f6d5ce3c654c5e560f32394d6dc1f06bb6f104
Way Lin <way.lin@jytek.tw>
2022-10-27 08:34:04 +0800

    add scripts

--- source/JYAutoTesterDEMO/Form1.Designer.cs
+++ source/JYAutoTesterDEMO/Form1.Designer.cs
@@ -209,7 +209,7 @@
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "JYAutoTesterDEMO";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Shown += new System.EventHandler(this.Form1_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pass)).EndInit();
